cd98a04555c36d3d3bc0297b55f43e8986907e9e
Aron Sharma <aron.sharma@shibsted.se>
2018-01-24 10:38:13 +0100

    review + list component added

---
+++ src/Review/Review.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Review extends Component {
+  render() {
+    return (
+      <div className="App">
+       
+        <p className="review">
+          This is a review
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/ReviewList/ReviewList.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+import Review from './Review';
+
+class Review extends Component {
+  render() {
+    return (
+      <div className="App">
+       
+        <p className="reviewList">
+          I want review
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
