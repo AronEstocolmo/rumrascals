05ce29a562d0aeb5af46ad27d0c157300fa42d4f
Felix Juréhn <felix.jurehn@schibsted.se>
2018-01-24 11:19:39 +0100

    initial setup of reviewlist and review

--- src/Review/Review.js
+++ src/Review/Review.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import Overdrive from 'react-overdrive';
 
-class Review extends Component {
-  render() {
-    return (
-      <div className="App">
-       
-        <p className="review">
-          This is a review
-        </p>
-      </div>
-    );
-  }
-}
+const POSTER_PATH = 'http://image.tmdb.org/t/p/w154';
 
-export default App;
+const Review = ({ review }) => (
+  <Link to={`/${review.id}`}>
+    <Overdrive id={review.id}>
+      <Poster src={`${POSTER_PATH}${review.poster_path}`} alt={review.title} />
+    </Overdrive>
+  </Link>
+);
+
+export default Review;
+
+Review.propTypes = {
+  review: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export const Poster = styled.img`
+  box-shadow: 0 0 35px black;
+`;
--- src/ReviewList/ReviewList.js
+++ src/ReviewList/ReviewList.js
@@ -2,17 +2,39 @@ import React, { Component } from 'react';
 import './App.css';
 import Review from './Review';
 
-class Review extends Component {
+const ALL_REVIEWS_PATH = "https://supercoolapi.sweet/reviews";
+
+class ReviewList extends PureComponent {
+  state = {
+    reviews: [],
+  }
+
+  async componentDidMount() {
+    try {
+      /*const res = await fetch('ALL_REVIEWS_PATH');
+      const reviews = await res.json();
+      this.setState({
+        reviews: reviews.results,
+      });*/
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-       
-        <p className="reviewList">
-          I want review
-        </p>
-      </div>
+      <ReviewList>
+        {this.state.reviews.map(review => <Review key={review.id} review={review} />)}
+      </ReviewList>
     );
   }
 }
 
-export default App;
+export default ReviewList;
+
+const ReviewList = styled.div`
+  display: list;
+  padding: 1rem;
+  grid-template-columns: repeat(6, 1fr);
+  grid-row-gap: 1rem;
+`;
